ef5f71b9c0c0ff95e69f7105a2398d8f4822a36d
Abdullah Zreqat <abdullah_zreqat@safeersoft.com>
2025-04-21 10:54:34 +0300

    update Cocoapods

--- bin/automate.dart
+++ bin/automate.dart
@@ -218,6 +218,7 @@ end
 
   Future<void> _handleBetaBuild(String platform, bool useFirebase) async {
     if (platform == 'ios') {
+      await _runCommand('cd ios && pod repo update', 'Updating CocoaPods repo');
       await _runCommand(
         'cd ios && pod install --repo-update',
         'Installing CocoaPods',
@@ -247,6 +248,8 @@ end
     await _incrementVersionAndBuildNumber();
 
     if (platform == 'ios') {
+      await _runCommand('cd ios && pod repo update', 'Updating CocoaPods repo');
+
       await _runCommand(
         'cd ios && pod install --repo-update',
         'Installing CocoaPods',
